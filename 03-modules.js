011080e359d42d940a6f46c7ee6cdb6b5a2ec730
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-20 23:09:59 +0500

    Move the variables from names file

--- 03-modules.js
+++ 03-modules.js
@@ -1,6 +1,6 @@
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
 
-const john = 'john'
-const peter = 'peter'
 const sayHi = (name) => {
     console.log(`Hello there ${name}`);
 }
