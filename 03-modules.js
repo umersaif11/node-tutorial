8a0d47c413525a248228f71f2dc26929c7cc1653
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-20 23:30:05 +0500

    Export func from utils and get it in modules

--- 03-modules.js
+++ 03-modules.js
@@ -1,6 +1,9 @@
 // CommonJS, every file is module (by default)
 // Modules - Encapsulated Code (only share minimum)
+const names  = require('./04-names')
+const sayHi = require('./05-utils')
+console.log(sayHi);
 
-sayHi('susan')
-sayHi(john)
-sayHi(peter)
+// sayHi('susan')
+// sayHi(john)
+// sayHi(peter)
--- utils.js
+++
@@ -1,3 +0,0 @@
-const sayHi = (name) => {
-    console.log(`Hello there ${name}`);
-}
