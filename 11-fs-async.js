7d4188885a486db3cc6ba47d3c3611003366a8bc
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-23 22:01:07 +0500

    Asynchornous file system and using its methods of read/write in callbacks...

---
+++ 11-fs-async.js
@@ -0,0 +1,28 @@
+const { readFile, writeFile } = require('fs')
+
+readFile('./content/first.txt', 'utf8', (err, resultOne ) => {
+    if(err) {
+        console.log(err)
+        return
+    }
+    const first = resultOne
+
+    readFile('./content/second.txt', 'utf8', (err, resultTwo ) => {
+        if(err) {
+            console.log(err)
+            return
+        }
+        const second = resultTwo
+
+        writeFile(
+            './content/result-async.txt',
+            `Here is the result: ${first}, ${second}`,
+            (err) => {
+                if(err) {
+                    console.log(err)
+                    return
+                }
+            }
+        )
+})
+})
