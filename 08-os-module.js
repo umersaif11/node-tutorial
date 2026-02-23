cd88766e9073f5ca81c7cc19fcf5d1b9aa73eff1
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-23 16:00:44 +0500

    Use another operating system's uptime method to check for how many seconds the OS is up

--- 08-os-module.js
+++ 08-os-module.js
@@ -1,4 +1,8 @@
 const os = require('os')
 
+//info about the current user
 const user = os.userInfo()
 console.log(user)
+
+//method returns the system uptime in seconds
+console.log(`The system uptime is ${os.uptime()} seconds`)
