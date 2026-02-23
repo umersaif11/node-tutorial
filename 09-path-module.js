2dc0b77bf557932e9a5d55cdb53cce800dd9887c
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-23 17:05:57 +0500

    Use basename method to get the base of filepath and resolve method to get the absolute path

--- 09-path-module.js
+++ 09-path-module.js
@@ -3,4 +3,10 @@ const path = require('path')
 console.log(path.sep)
 
 const filePath = path.join('./content', 'sub-folder', 'test.txt')
-console.log(filePath) 
+console.log(filePath)
+
+const base = path.basename(filePath)
+console.log(base)
+
+const absolute = path.resolve(__dirname, 'content', 'sub-folder', 'test.txt')
+console.log(absolute)
