6a9b43eb5959d029da8308d41084cfd4f0f8db1d
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-24 17:12:33 +0500

    Refactor http code and uninstall some packages

--- 12-http.js
+++ 12-http.js
@@ -1,19 +1,29 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
+    // if(req.url === '/') {
+    //     res.end('Welcome to our homepage')
+    //     return
+    // }
+    // if(req.url === '/about') {
+    //     res.end('Here is our short history')
+    //     return
+    // }
+    // res.end(
+    //     `<h1>Ooops!</h1>
+    //      <p>We can't seem to find page you are looking for</p>
+    //      <a href='/'>Back Home</a>`
+    // )
+    // return
     if(req.url === '/') {
         res.end('Welcome to our homepage')
-        return
-    }
-    if(req.url === '/about') {
+    }else if(req.url === '/about') {
         res.end('Here is our short history')
-        return
-    }
-    res.end(
+    } else {
+        res.end(
         `<h1>Ooops!</h1>
          <p>We can't seem to find page you are looking for</p>
          <a href='/'>Back Home</a>`
-    )
-    return
+    )}
 })
 server.listen(5000)
