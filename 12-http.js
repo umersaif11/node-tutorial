a8f5cfed70ddf02ce7792f176ea9af0e1fbd8ee5
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-24 15:18:47 +0500

    Using the url property of request object for routing the website

--- 12-http.js
+++ 12-http.js
@@ -1,7 +1,19 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    res.write('Welcome to our homepage')
-    res.end()
+    if(req.url === '/') {
+        res.end('Welcome to our homepage')
+        return
+    }
+    if(req.url === '/about') {
+        res.end('Here is our short history')
+        return
+    }
+    res.end(
+        `<h1>Ooops!</h1>
+         <p>We can't seem to find page you are looking for</p>
+         <a href='/'>Back Home</a>`
+    )
+    return
 })
 server.listen(5000)
