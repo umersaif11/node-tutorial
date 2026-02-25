e54d2bb21c4cd4751cb7747090c449acd38d8428
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-25 21:35:38 +0500

    So to get rid of getText as we will now use util module's promisfy method which will convert readFile into promise

--- 02-async-patterns/02-await-patterns.js
+++ 02-async-patterns/02-await-patterns.js
@@ -1,28 +1,43 @@
 const { readFile, writeFile } = require('fs')
 
-const getText = (path) => {
-    return new Promise((resolve, reject) => {
-        readFile(path, 'utf8', (err, result) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(result)
-            }
-        })
-    })
-}
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf8', (err, result) => {
+//             if (err) {
+//                 reject(err)
+//             } else {
+//                 resolve(result)
+//             }
+//         })
+//     })
+// }
 
-//using promise
+// (1) using promise
 // getText('./content/first.txt')
 // .then(result => console.log(result))
 // .catch(error => console.log(error))
 
 
-//using async await
+// (2) using async await
+// const start = async() => {
+//     try {
+//         const first = await getText('./content/first.txt', 'utf8')
+//         const second = await getText('./content/second.txt', 'utf8')
+//         console.log(first, second)
+//     } catch(error) {
+//         console.log(error)
+//     }
+// }
+// start()
+
+// (3) async await.... util promisfy 
+const util = require('util')
+const readFilePromise = util.promisify(readFile)
 const start = async() => {
     try {
-        const first = await getText('./content/first.txt')
-        console.log(first)
+        const first = await readFilePromise('./content/first.txt', 'utf8')
+        const second = await readFilePromise('./content/second.txt', 'utf8')
+        console.log(first, second)
     } catch(error) {
         console.log(error)
     }
