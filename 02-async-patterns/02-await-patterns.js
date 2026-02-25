83299a7b5662f2784dc783a8267f4e5c0e0a15cd
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-25 21:50:32 +0500

    Flag a added which will append the file

--- 02-async-patterns/02-await-patterns.js
+++ 02-async-patterns/02-await-patterns.js
@@ -48,14 +48,15 @@ const { readFile, writeFile } = require('fs').promises
 // }
 // start()
 
-//(4) sync...await   require('fs').promises
+//(4) async...await   require('fs').promises
 const start = async() => {
     try {
         const first = await readFile('./content/first.txt', 'utf8')
         const second = await readFile('./content/second.txt', 'utf8')
         await writeFile(
             './content/result-async-await.txt',
-            `This is awesome: ${first}, ${second}`
+            `This is awesome: ${first}, ${second}`,
+            { flag: 'a' }
         )
     } catch(error) {
         console.log(error)
