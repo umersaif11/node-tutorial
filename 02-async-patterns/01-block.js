2334be40e8f8dbdaf6fdcc3a8fec150a74074b67
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-25 15:21:55 +0500

    Putting blocking code of nested for loops within async code

---
+++ 02-async-patterns/01-block.js
@@ -0,0 +1,19 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.end('Home page')
+    }else if(req.url === '/about') {
+        for(let i = 0; i < 1000; i++) {
+            for(let j = 0; j < 1000; j++) {
+                console.log(`${i} ${j}`)
+            }
+        }
+        res.end('About page')
+    }else {
+        res.end('Error Page')
+    }
+})
+server.listen(5000, () => {
+    console.log('Server listening on port 5000')
+})
