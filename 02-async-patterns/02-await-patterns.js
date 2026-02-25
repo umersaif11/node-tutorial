51bf55027819363e008b781abef7eb4b1a9a6682
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-25 21:42:45 +0500

    So to get rid of getText as we will now use util module's promisfy method which will convert readFile and writefile into promise...

--- 02-async-patterns/02-await-patterns.js
+++ 02-async-patterns/02-await-patterns.js
@@ -30,14 +30,18 @@ const { readFile, writeFile } = require('fs')
 // }
 // start()
 
-// (3) async await.... util promisfy 
+// (3) async await.... util.promisfy 
 const util = require('util')
 const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile)
 const start = async() => {
     try {
         const first = await readFilePromise('./content/first.txt', 'utf8')
         const second = await readFilePromise('./content/second.txt', 'utf8')
-        console.log(first, second)
+        await writeFilePromise(
+            './content/result-async-await.txt',
+            `This is awesome: ${first}, ${second}`
+        )
     } catch(error) {
         console.log(error)
     }
