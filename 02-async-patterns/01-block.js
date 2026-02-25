63808ad5d63575afcb4dedcc7d5bd83535f20998
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-25 15:24:06 +0500

    Putting blocking code of nested for loops within async code

--- 02-async-patterns/01-block.js
+++ 02-async-patterns/01-block.js
@@ -4,6 +4,7 @@ const server = http.createServer((req, res) => {
     if(req.url === '/') {
         res.end('Home page')
     }else if(req.url === '/about') {
+        //Blocking code
         for(let i = 0; i < 1000; i++) {
             for(let j = 0; j < 1000; j++) {
                 console.log(`${i} ${j}`)
