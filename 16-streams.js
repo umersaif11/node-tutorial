c308ecfd75083a5294eb79e110e7d1dd911fcb8a
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-26 16:36:48 +0500

    HighWaterMark for controlling size and encoding to read the real content

--- 16-streams.js
+++ 16-streams.js
@@ -1,6 +1,14 @@
 const { createReadStream } = require('fs')
 
-const stream = createReadStream('./content/big.txt')
+const stream = createReadStream('./content/big.txt', {
+    highWaterMark: 15000,
+    encoding: 'utf8'
+})
+
+//default - 64kb
+//last buffer -  remainder
+//highWaterMark - control size
+//const stream = createReadStream('./content/big.txt', {highWaterMark: 15000})
 
 
 stream.on('data', (result) => {
