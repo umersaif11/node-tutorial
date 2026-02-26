911b941e58bad02a714728fcff8cd538bf7968e7
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-26 16:38:52 +0500

    Event for checking error

--- 16-streams.js
+++ 16-streams.js
@@ -9,8 +9,10 @@ const stream = createReadStream('./content/big.txt', {
 //last buffer -  remainder
 //highWaterMark - control size
 //const stream = createReadStream('./content/big.txt', {highWaterMark: 15000})
-
+////const stream = createReadStream('./content/big.txt', {encoding: 'utf8})
 
 stream.on('data', (result) => {
     console.log(result)
 })
+
+stream.on('error', (err) => console.log(err))
